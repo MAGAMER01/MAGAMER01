3069b159c25b633c47841e714535c4787d04ca61
inrl-official <fasweehmon2@gmail.com>
2023-07-12 07:37:32 +0530

    Update youtube.js

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -22,15 +22,15 @@ try {
     const {data} = await axios(BASE_URL+`api/yts?text=${match}`);
     const {result} = data
     if(!result) return await m.send('_not found_');
-    let msg ="*YT SONG DOWNLOADER*\n\n", n =1;
-    await result.map(r=>msg += `${n++}. `+'```'+`${r.title}`+'```\n');
+    let msg ="*_YT SONG DOWNLOADER_*\n\n", n =1;
+    await result.map(r=>msg += `*${n++}*. `+'```'+`${r.title}`+'```\n');
     return await m.send(msg);
    } else {
    m.client.text = url[0]
     return await downloadYT(m, m.conn,'mp3');
    }
    } catch(e){
-   return m.send('_Time Out_'+e);
+   return m.send('_Time Out_ '+e);
    }
 });
 
@@ -46,8 +46,8 @@ try {
     const {data} = await axios(BASE_URL+`api/yts?text=${match}`);
     const {result} = data
     if(!result) return await m.send('_not found_');
-    let msg ="*YT VIDEO DOWNLOADER*\n\n", n =1;
-    await result.map(r=>msg += `${n++}. `+'```'+`${r.title}`+'```\n');
+    let msg ="*_YT VIDEO DOWNLOADER_*\n\n", n =1;
+    await result.map(r=>msg += `*${n++}*. `+'```'+`${r.title}`+'```\n');
     return await m.send(msg);
    } else {
     m.client.text = url[0]
@@ -65,15 +65,15 @@ inrl({
 if(!m.quoted || !m.quoted.fromMe) return;
 try {
 if(m.client.body.includes("YT VIDEO DOWNLOADER")){
-match = m.client.body.replace("YT VIDEO DOWNLOADER").trim();
-await m.send(`_downloading_\n*_${match}_*`);
+match = m.client.body.replace("YT VIDEO DOWNLOADER","").trim();
+await m.send(`*_downloading_*\n*_${match}_*`);
 const {data} = await axios(BASE_URL+`api/yts?text=${match}`);
 const {result} = data;
 m.client.text= result[0].url;
 return await downloadYT(m, m.conn,'mp4');
 } else if(m.client.body.includes("YT SONG DOWNLOADER")){
-match = m.client.body.replace("YT SONG DOWNLOADER").trim();
-await m.send(`_downloading_\n${match}`);
+match = m.client.body.replace("YT SONG DOWNLOADER","").trim();
+await m.send(`*_downloading_*\n${match}`);
 const {data} = await axios(BASE_URL+`api/yts?text=${match}`);
 const {result} = data;
 m.client.text= result[0].url;

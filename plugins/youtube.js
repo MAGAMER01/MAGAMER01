8356918ce19b105ba35a664c20dac778fa874f68
inrl-official <fasweehmon2@gmail.com>
2023-07-13 08:12:27 +0530

    Update youtube.js

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -1,9 +1,10 @@
 //created by @inrl
-const { downloadYT } = require('inrl-bot-md');
-const axios = require('axios');
+const yt = require('inrl-bot-md');
+const fs = require('fs');
 
 const {
     inrl,
+    sleep,
     extractUrlsFromString
 } = require('../lib/');
 const {
@@ -19,15 +20,21 @@ try {
     if(!match) return await m.send("*_give me url or quarry_*");
     const  url = await extractUrlsFromString(match);
     if(!url[0]){
-    const {data} = await axios(BASE_URL+`api/yts?text=${match}`);
-    const {result} = data
+    const result = await yt.search(match);
     if(!result) return await m.send('_not found_');
     let msg ="*_YT SONG DOWNLOADER_*\n\n", n =1;
     await result.map(r=>msg += `*${n++}*. `+'```'+`${r.title}`+'```\n');
     return await m.send(msg);
    } else {
    m.client.text = url[0]
-    return await downloadYT(m, m.conn,'mp3');
+    const media = await yt.stream(url[0],{
+        quality: 'medium',
+        type: 'audio',
+        highWaterMark: 1048576 * 32
+    });
+    file = await media.stream.pipe(fs.createWriteStream('./type.mp3'));
+    await sleep(100);
+    return await m.conn.sendMessage(m.from, {audio:fs.readFileSync('./type.mp3'),mimetype: 'audio/mpeg'});
    }
    } catch(e){
    return m.send('_Time Out_ '+e);
@@ -43,15 +50,21 @@ try {
     if(!match) return await m.send("*_give me url or quarry_*");
     const  url = await extractUrlsFromString(match);
     if(!url[0]){
-    const {data} = await axios(BASE_URL+`api/yts?text=${match}`);
-    const {result} = data
+    const result = await yt.search(match);
     if(!result) return await m.send('_not found_');
     let msg ="*_YT VIDEO DOWNLOADER_*\n\n", n =1;
     await result.map(r=>msg += `*${n++}*. `+'```'+`${r.title}`+'```\n');
     return await m.send(msg);
    } else {
     m.client.text = url[0]
-    return await downloadYT(m, m.conn,'mp4');
+    const media = await yt.stream(url[0],{
+        quality: 'medium',
+        type: 'video',
+        highWaterMark: 1048576 * 32
+    });
+    file = await media.stream.pipe(fs.createWriteStream('./type.mp4'));
+    await sleep(100);
+    return await m.conn.sendMessage(m.from, {video:fs.readFileSync('./type.mp4'),mimetype: 'video/mp4'});
    }
    } catch(e){
    return m.send('_Time Out_'+e);
@@ -67,17 +80,27 @@ try {
 if(m.client.body.includes("YT VIDEO DOWNLOADER")){
 match = m.client.body.replace("YT VIDEO DOWNLOADER","").trim();
 await m.send(`*_downloading_*\n*_${match}_*`);
-const {data} = await axios(BASE_URL+`api/yts?text=${match}`);
-const {result} = data;
-m.client.text= result[0].url;
-return await downloadYT(m, m.conn,'mp4');
+const result = await yt.search(match);
+const media = await yt.stream(result[0].url, {
+        quality: 'medium',
+        type: 'video',
+        highWaterMark: 1048576 * 32
+    });
+    file = await media.stream.pipe(fs.createWriteStream('./type.mp4'));
+    await sleep(100);
+    return await m.conn.sendMessage(m.from, {video:fs.readFileSync('./type.mp4'),mimetype: 'video/mp4'});
 } else if(m.client.body.includes("YT SONG DOWNLOADER")){
 match = m.client.body.replace("YT SONG DOWNLOADER","").trim();
 await m.send(`*_downloading_*\n${match}`);
-const {data} = await axios(BASE_URL+`api/yts?text=${match}`);
-const {result} = data;
-m.client.text= result[0].url;
-return await downloadYT(m, m.conn,'mp3');
+const result = await yt.search(match);
+const media = await yt.stream(result[0].url, {
+        quality: 'medium',
+        type: 'audio',
+        highWaterMark: 1048576 * 32
+    });
+    file = await media.stream.pipe(fs.createWriteStream('./type.mp3'));
+    await sleep(100);
+    return await m.conn.sendMessage(m.from, {audio:fs.readFileSync('./type.mp3'),mimetype: 'audio/mpeg'});
 }
 } catch(e){
 return await m.send('_Error, try again!_')
